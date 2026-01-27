6c2881aad10584031bba5924eba844ac73fca1ec
Lucky44 <tripp3@gmail.com>
2026-01-27 09:52:12 -0800

    Fix Reclaimer point defense turrets (S2→S1)
    Replace last 7 CF-227 Badger Repeaters (S2) with M3A Cannons (S1)
    to match S1 point defense turret mounts.
    
    Issues reduced from 86 to 79

--- data.js
+++ data.js
@@ -2075,7 +2075,7 @@ SC_DATA.stockLoadouts = {
         coolers: ["Algid"],
         shields: ["RS-Barrier"],
         pilotWeapons: [],
-        turretWeapons: ["M2C \"Swarm\"","CF-337 Panther Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater"]
+        turretWeapons: ["M2C \"Swarm\"","CF-337 Panther Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater","CF-227 Badger Repeater","M3A Cannon","M3A Cannon","M3A Cannon","M3A Cannon","M3A Cannon","M3A Cannon","M3A Cannon"]
     },
     "Aegis Reclaimer 2949 Best In Show Edition": {
         quantumDrives: ["Kama"],
